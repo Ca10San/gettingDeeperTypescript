a33f3ce970af2df17bdb149401d77254e39f0d63
Ca10San <caiodarkus2@gmail.com>
2022-04-13 18:01:58 -0300

    :sparkles: add new view

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
+import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
@@ -6,3 +7,8 @@ form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+
+const negociacoesView = new NegociacoesView;
+const template = negociacoesView.template();
+const negociacoesViewElement = document.querySelector("#negociacoesView");
+negociacoesViewElement.innerHTML = template;
--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,12 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
+import { NegociacoesView } from './views/negociacoes-view.js';
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
 });
+const negociacoesView = new NegociacoesView;
+const template = negociacoesView.template();
+const negociacoesViewElement = document.querySelector("#negociacoesView");
+negociacoesViewElement.innerHTML = template;
