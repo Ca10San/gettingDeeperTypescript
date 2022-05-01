3db33dd6916daa1a3c5b221fde046574b939da99
Ca10San <caiodarkus2@gmail.com>
2022-05-01 13:56:09 -0300

    :rotating_light: treatening null returns

--- app/app.ts
+++ app/app.ts
@@ -1,9 +1,13 @@
 import { NegociacaoController } from './controllers/negociacao-controller.js';
-import { NegociacoesView } from './views/negociacoes-view.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    controller.adiciona();
-});
+
+if(form) {
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        controller.adiciona();
+    });
+} else {
+    throw Error("Form não encontrado! Não foi possível inicializar a aplicação.")
+}
--- app/views/view.ts
+++ app/views/view.ts
@@ -1,13 +1,27 @@
 export abstract class View<T> {
     protected element: HTMLElement;
+    private escapar: boolean = false;
 
-    constructor(seletor: string) {
-        this.element = document.querySelector(seletor);
+    constructor(seletor: string, escapar?: boolean) {
+        const element = document.querySelector(seletor); 
+        if(element) {            
+            this.element = element as HTMLElement;
+        } else {
+            throw Error(`Seletor ${seletor} não existe no DOM. Verifique`)
+        }
+        if(escapar) {
+            this.escapar = escapar;
+        }
     }
 
     protected abstract template(model: T): string;
 
     public update(model: T): void {
-        this.element.innerHTML = this.template(model);
+        let template = this.template(model);
+        if(this.escapar) {
+            template = template
+            .replace(/<script>[\s\S]*?<\/script>/, '');
+        }
+        this.element.innerHTML = template;
     }
 }
